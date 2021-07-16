d255583b98a54647337a03ab99e9c5cef8d38375
Yu Jia Liao <kevinc0227@gmail.com>
2021-07-16 20:58:01 +0800

    add modal

--- t4_readpdf/src/App.js
+++ t4_readpdf/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import 'antd/dist/antd.css';
+import { Modal, Button } from 'antd';
 // import Readpdf from './components/Readpdf'
 import SinglePagePDFViewer from './components/single-page'
 import AllPagesPDFViewer from './components/all-pages';
@@ -6,19 +8,39 @@ import TAO from './sample.pdf'
 import './App.css';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <React.Fragment>
-      <h4>Single Page</h4>
-      <SinglePagePDFViewer pdf={TAO} />
+      <Button type="primary" onClick={showModal}>
+        點擊閱讀pdf
+      </Button>
+      <Modal title="PDF預覽" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <h4>Single Page</h4>
+        <SinglePagePDFViewer pdf={TAO} />
 
-      <hr />
+        <hr />
 
-      <h4>All Pages</h4>
-      <div className="all-page-container">
-        <AllPagesPDFViewer pdf={TAO} />
-      </div>
+        <h4>All Pages</h4>
+        <div className="all-page-container">
+          <AllPagesPDFViewer pdf={TAO} />
+        </div>
 
-      <hr />
+        <hr />
+      </Modal>
+      
       {/* <Readpdf></Readpdf> */}
     </React.Fragment>
   );
