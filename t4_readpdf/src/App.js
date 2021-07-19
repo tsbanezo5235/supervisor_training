116758d2a46fd03755cd1f41a4366476bba6e9b6
Yu Jia Liao <kevinc0227@gmail.com>
2021-07-19 09:57:05 +0800

    complete t4

--- t4_readpdf/src/App.js
+++ t4_readpdf/src/App.js
@@ -3,7 +3,6 @@ import 'antd/dist/antd.css';
 import { Modal, Button } from 'antd';
 // import Readpdf from './components/Readpdf'
 import SinglePagePDFViewer from './components/single-page'
-import AllPagesPDFViewer from './components/all-pages';
 import TAO from './sample.pdf'
 import './App.css';
 
@@ -27,16 +26,9 @@ function App() {
       <Button type="primary" onClick={showModal}>
         點擊閱讀pdf
       </Button>
-      <Modal title="PDF預覽" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Modal title="PDF預覽" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width={700}>
         <h4>Single Page</h4>
         <SinglePagePDFViewer pdf={TAO} />
-
-        <hr />
-
-        <h4>All Pages</h4>
-        <div className="all-page-container">
-          <AllPagesPDFViewer pdf={TAO} />
-        </div>
 
         <hr />
       </Modal>
--- t4_readpdf/src/components/single-page.js
+++ t4_readpdf/src/components/single-page.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import 'antd/dist/antd.css';
+import { InputNumber, Button, Space } from 'antd';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const SinglePage = (props) => {
@@ -27,6 +29,20 @@ const SinglePage = (props) => {
 
   return (
     <React.Fragment>
+      <Space>
+        <InputNumber min={1} max={10} value={pageNumber} onChange={setPageNumber} />
+        {/* <Button
+          type="primary"
+          onClick={() => {
+            setPageNumber(99);
+          }}
+        >
+          跳轉
+        </Button> */}
+        <p>
+          Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+        </p>
+      </Space>
       <Document
         file={pdf}
         options={{ workerSrc: "/pdf.worker.js" }}
